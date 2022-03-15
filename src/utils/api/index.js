bd2727ed1a587d345fc7ccc6b53fb6880e1a89c2
luobozz <437337961@qq.com>
2022-03-15 11:56:34 +0800

    wip: resultHandle
    line:74

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -25,15 +25,59 @@ export default {
     },
     resultHandle(res, custom) {
         return new Promise((resolve, reject) => {
-            const statusHandle={
-                403:()=>{},
-                401:()=>{},
-                500:()=>{},
-                400:()=>{},
-                default:()=>{}
+            const resData = res.data || {}, afterHandle = (isSuccess, isToast, toastContent) => {
+                return {
+                    isSuccess,
+                    isToast,
+                    toastContent
+                }
             }
-            const f=statusHandle[res.status]||statusHandle.default
-            console.log(f)
+                , statusHandle = {
+                401: () => {
+                    //状态码 401 登录权限 先刷新token，如果还失败跳回登录页
+                    return afterHandle(false, true, "401")
+                },
+                403: () => {
+                    //状态码 403 接口权限 提示无权限
+                    return afterHandle(false, true, "接口无权限")
+                },
+                500: () => {
+                    //状态码 500 统一提示 服务器异常，不输出msg
+                    return afterHandle(false, false, null)
+                },
+                200: () => {
+                    //状态码 200 succeed:false 业务异常，直接输出msg
+                    //状态码 200 succeed:true 成功，直接输出msg
+                    return afterHandle(resData.succeed || false, true, resData.msg || "业务异常")
+                },
+                400: () => {
+                    //状态码 400 参数解析异常 可以直接输出msg
+                    return afterHandle(false, true, resData.msg || "参数解析异常")
+                },
+                default: () => {
+                    //其他状态码，输出异常(网络错误)
+                    return afterHandle(false, true, "网络错误")
+                }
+            }
+            const f = statusHandle[res.status] || statusHandle.default
+            const fr = f();
+            if (fr.isSuccess) {
+                if (!(custom?.noToast?.all || custom?.noToast.success) && fr.isToast) {
+                    uiUtils.toast({
+                        type: lodash.isEmpty(resData.data) ? "warning" : "success",
+                        message: fr.toastContent
+                    })
+                }
+                resolve(resData)
+            } else {
+                if (!(custom?.noToast?.all || custom?.noToast?.error) && fr.isToast) {
+                    exception.toastError(res, false);
+                } else {
+                    exception.silentError(`${res.code}:${res.message}`)
+                }
+                reject(res)
+            }
+            console.log(f())
             // if (res.code == 200 || res.code == 404) {
             //     const type = res.code == 200 ? "success" : "warning"
             //     resolve(res);
@@ -72,7 +116,7 @@ export default {
                 message: `${url} 连接成功`
             })
         }).catch(err => {
-            exception.toastError(`连接失败,事由[${err}]`);
+            exception.toastError(`连接失败,事由[${err}]`,false);
         })
     }
 
